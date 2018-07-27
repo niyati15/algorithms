e70a47529e3d8e467b73f187a9f227c5494e06a3
Niyati <niyati92@gmail.com>
2018-07-27 16:53:09 -0700

    linkedlist exxentials done

--- data-structures/linked-list/linked-list.js
+++ data-structures/linked-list/linked-list.js
@@ -1,22 +1,34 @@
+// *************************************************** LINKED LIST ESSENTIALS ***************************************************
 //Linkedlist is a datastructure with nodes and each node has a previous pointer and a next pointer
+//Time Complexity:
+//constant time for adding/removing head or tail
+//O(n) for searching through a linked list
+//advantages:
+//can be stored in an efficient way in the memory. sometimes all the data cannot be stored together, here linkedlist can be 
+//store in different locations and can be connected using pointers.
+//also, its time complexity is pretty efficient.
+// ****************************************************************************************************************************
+
+
+
 //constructor for linkedlist
-function LinkedList(){
+function LinkedList() {
     this.head = null;
     this.tail = null;
-}
+};
 //constructor for node
-function Node(value, next, prev){
+function Node(value, next, prev) {
     this.value = value;
     this.next = next;
     this.prev = prev;
-}
+};
 //add to head 
-LinkedList.prototype.addToHead = function(value){
+LinkedList.prototype.addToHead = function (value) {
     var newNode = new Node(value, this.head, null);
-    if(this.head) this.head.prev = newNode;
+    if (this.head) this.head.prev = newNode;
     else this.tail = newNode;
     this.head = newNode;
-}
+};
 
 // var ll = new LinkedList();
 
@@ -27,12 +39,12 @@ LinkedList.prototype.addToHead = function(value){
 // console.log(ll);
 
 //add to tail
-LinkedList.prototype.addToTail = function(value){
+LinkedList.prototype.addToTail = function (value) {
     var newNode = new Node(value, null, this.tail);
-    if(this.tail) this.tail.next = newNode;
+    if (this.tail) this.tail.next = newNode;
     else this.head = newNode;
     this.tail = newNode;
-}
+};
 
 // var ll1 = new LinkedList();
 
@@ -43,11 +55,56 @@ LinkedList.prototype.addToTail = function(value){
 // console.log(ll1);
 
 //Remove head from linkedlist
-LinkedList.prototype.removeHead = function(){
-    if(!this.head) return null;
+LinkedList.prototype.removeHead = function () {
+    if (!this.head) return null;
     var val = this.head.value;
     this.head = this.head.next;
-    if(this.head) this.head.prev = null;
+    if (this.head) this.head.prev = null;
     else this.tail = null;
     return val;
-}
+};
+
+
+//Remove tail from linked list
+LinkedList.prototype.removeTail = function () {
+    if (!this.tail) return null;
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) this.tail.next = null;
+    else this.head = null;
+    return val;
+};
+
+//Search in a linked list
+LinkedList.prototype.search = function (searchValue) {
+    var currentNode = this.head;
+    while (currentNode) {
+        if (currentNode.value === this.searchValue) return currentNode.value;
+        currentNode = currentNode.next;
+    }
+    return null;
+};
+
+//search the index of a value in linked list - indexOf() 
+LinkedList.prototype.indexOf = function (searchValue) {
+    var currentNode = this.head;
+    var indexes = [];
+    var currentIndex = 0;
+    while (currentNode) {
+        if(currentNode.value === searchValue){
+            indexes.push(currentIndex);
+        }
+        currentNode = currentNode.next;
+        currentIndex++;
+    }
+    return indexes;
+};
+
+
+// var ll = new LinkedList();
+// ll.addToTail(100);
+// ll.addToTail(300);
+// ll.addToTail(300);
+// ll.addToTail(400);
+
+// console.log(ll.indexOf(300));
