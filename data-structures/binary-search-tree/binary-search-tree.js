d93bc1d1d91d66fb2d52a181d2242f9a6ca9913b
Niyati <niyati92@gmail.com>
2018-07-27 18:32:57 -0700

    BST insert and contains methods added

--- data-structures/binary-search-tree/binary-search-tree.js
+++ data-structures/binary-search-tree/binary-search-tree.js
@@ -2,4 +2,55 @@
 // BST is a collection of nodes where the nodes are connected in a certain way
 // Each has 2 child nodes - a left node and a right node
 // All the left nodes are to less or equal value to the parent node and all right nodes are of greater value than that of the parent
-//Consider all the nodes as individual BST *************************************************************************************************************************************
+//Consider all the nodes as individual BST 
+// *************************************************************************************************************************************
+
+//constructor function for BST
+function BST(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+//Insert nodes into a BST
+BST.prototype.insert = function(value){
+    if(value <= this.value){
+        if(!this.left) this.left = new BST(value);
+        else this.left.insert(value);
+    }
+    else if(value > this.value){
+        if(!this.right) this.right = new BST(value);
+        else this.right.insert(value);
+    }
+}
+
+//Contains function to check if the BST contains that value
+BST.prototype.contains = function(value){
+    if(value === this.value) return true;
+    else if(value < this.value){
+        if(!this.left) return false;
+        else return this.left.contains(value);
+    }
+    else if(value > this.value){
+        if(!this.right) return false;
+        else return this.right.contains(value);
+    }
+}
+
+// var bst = new BST(50);
+
+// bst.insert(30);
+// bst.insert(20);
+// bst.insert(10);
+// bst.insert(45);
+// bst.insert(35);
+// bst.insert(70);
+// bst.insert(100);
+// bst.insert(60);
+// bst.insert(59);
+// bst.insert(85);
+// bst.insert(105);
+
+// console.log(bst);
+
+// console.log(bst.contains(100001));
